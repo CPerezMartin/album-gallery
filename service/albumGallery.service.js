1e27f37d50c1c5b7e776cfa55f83dee647019153
Carlos Pérez Martín <39385318+CPerezMartin@users.noreply.github.com>
2018-11-12 23:40:11 +0100

    services finished, ready for testing

--- service/albumGallery.service.js
+++ service/albumGallery.service.js
@@ -17,9 +17,39 @@ app.service('$gallerySrv', [
           } else {
             reject('error');
           }
-        });
+        });        
+      })
+    }
 
-        
+    vm.getAlbums = function (albumId) {
+      console.log('retrieving users');
+      const url = 'http://jsonplaceholder.typicode.com/albums?';
+
+      return $q(function (resolve, reject) {
+        $http.get(url,albumId).success(function (data) {
+
+          if (data) {
+            resolve(data);
+          } else {
+            reject('error');
+          }
+        });        
+      })
+    }
+
+    vm.getPhotos = function (photoId) {
+      console.log('retrieving users');
+      const url = 'http://jsonplaceholder.typicode.com/albums?';
+
+      return $q(function (resolve, reject) {
+        $http.get(url,photoId).success(function (data) {
+
+          if (data) {
+            resolve(data);
+          } else {
+            reject('error');
+          }
+        });        
       })
     }
   }
